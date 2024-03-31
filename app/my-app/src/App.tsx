8b2a2d64d5e3c68c74fca1126d69a0bf1079d36e
masaharu <uchiyama.masaharu999@gmail.com>
2024-03-31 15:17:29 +0900

    ランダム画像表示機能の実装

--- app/my-app/src/App.tsx
+++ app/my-app/src/App.tsx
@@ -1,24 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
+  const [imgUrl, setImgUrl] = useState("https://source.unsplash.com/random");
+
+  const onClickRandom = () => {
+    axios
+      .get("https://source.unsplash.com/random", { responseType: "blob" })
+      .then((res) => setImgUrl(URL.createObjectURL(res.data)));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div style={{ display: "flex", flexFlow: "column" }}>
+        <button
+          style={{ width: "80px", margin: "5px auto 0 auto" }}
+          onClick={onClickRandom}
         >
-          Learn React
-        </a>
-      </header>
+          ランダム
+        </button>
+        <img src={imgUrl} alt="ランダム画像" style={{ height: "95vh" }} />
+      </div>
     </div>
   );
 }
